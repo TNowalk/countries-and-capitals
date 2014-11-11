ea048c810f94bea18778ffa776beec334be2b5af
SIMON TUCKER <srtucker22@gmail.com>
2014-11-10 17:02:36 -0800

    basic gulpfile added

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,71 @@
-//inside of gulpfile.js
+// gulp
 var gulp = require('gulp');
-var $ = require('gulp-load-plugins')(
-  {
-    camelize: true
-  });
 
-gulp.task('minify-views', function() {
-  var opts = {comments: true, empty: true, spare: true};
+// plugins
+var connect = require('gulp-connect');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+var clean = require('gulp-clean');
 
-  gulp.src('./app/tempates/*.html')
-    .pipe($.minifyHtml(opts))
-    .pipe(gulp.dest('dist/tempates/'));
+// tasks
+gulp.task('lint', function() {
+  gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
 });
 
-function jshint(src){
-  gulp.src(src)
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('default'));
-}
-
-gulp.task('jshint-source', function() {
-  jshint(['./app/js/*.js', './app/js/**/*.js']);
+gulp.task('clean', function() {
+    gulp.src('./dist/*')
+      .pipe(clean({force: true}));
 });
 
-gulp.task('jshint-dist', function(){
-  jshint('./dist/js/*.js');
+gulp.task('minify-css', function() {
+  var opts = {comments:true,spare:true};
+  gulp.src(['./app/**/*.css', '!./app/bower_components/**'])
+    .pipe(minifyCSS(opts))
+    .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('minify-css-js', function(){
-  var opts = {comments: true, empty: true, spare: true};
-
-  gulp.src('./app/*.html')
-    .pipe($.usemin({
-      css: [$.minifyCss(), 'concat'],
-      html: [$.minifyHtml(opts)],
-      js: [$.uglify()]
+gulp.task('minify-js', function() {
+  gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
+    .pipe(uglify({
+      // inSourceMap:
+      // outSourceMap: "app.js.map"
     }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('copy-bower-components', function () {
+  gulp.src('./app/bower_components/**')
+    .pipe(gulp.dest('dist/bower_components'));
+});
+gulp.task('copy-html-files', function () {
+  gulp.src('./app/**/*.html')
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(['app/*.html', 
-              'app/tempalates/*.html',
-              'app/css/*.css', 
-              'app/js/*.js',
-              'app/js/**/*.js'], ['jshint-source', 'minify-views', 'minify-css-js', 'jshint-dist']);
+gulp.task('connect', function () {
+  connect.server({
+    root: 'app/',
+    port: 8888
+  });
 });
 
-// Default Task
-gulp.task('default', ['watch']);
+gulp.task('connectDist', function () {
+  connect.server({
+    root: 'dist/',
+    port: 9999
+  });
+});
+
+
+// default task
+gulp.task('default',
+  ['lint', 'connect']
+);
+// build task
+gulp.task('build',
+  ['lint', 'minify-css', 'minify-js', 'copy-html-files', 'copy-bower-components', 'connectDist']
+);
